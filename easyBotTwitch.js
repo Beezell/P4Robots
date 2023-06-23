2261afe38eddea1b119070debacfc81d4544b203
Beezell <abigael.castillo@live.fr>
2023-06-23 19:53:34 +0200

    Robossein avec des petites méthodes trop cool

--- easyBotTwitch.js
+++ easyBotTwitch.js
@@ -23,14 +23,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 --------------------------------------------------------------------
 */
 const mongoose_1 = __importDefault(require("mongoose"));
-// J'ajoute cette ligne pour augmenter la limite d'écouteurs d'événements - SERT A RIEN POUR LE MOMENT
-//mongoose.connection.setMaxListeners(0);
-//Interface qui me sert pour mongoose
-// interface IGameTop {
-//   id: string;
-//   name: string;
-//   igdbId: string;
-// }
 //Je crée un schéma qui correspond à mon interface
 const gameSchema = new mongoose_1.default.Schema({
     _id: String,
@@ -47,85 +39,102 @@ class GameTopObject {
         this.igdbId = igdbId;
     }
 }
-//ma petite liste de games pepouse
-//var gameTopArray: GameTopObject[] = [];
+//Méthode qui cherche mes Games et qui les enregistre sur MongoDB
 function fetchGame() {
     return __awaiter(this, void 0, void 0, function* () {
-        // Pour twitch besoin d'Authorization et de clientId
-        let authorization = "Bearer 7tb61t29r3fhaft6ux6hh64fr1sf6v";
-        let clientId = "7xereixlp03cyd9lsebf4om6rensrb";
-        //La requête GET topGames
-        let api = "https://api.twitch.tv/helix/games/top";
-        let url = `${api}`;
+        //Token pour l'API Twhitch et IGDB
+        const authorization = "Bearer 7tb61t29r3fhaft6ux6hh64fr1sf6v";
+        const clientId = "7xereixlp03cyd9lsebf4om6rensrb";
+        //Requête Get TopGames API Twitch
+        const urlGetTopGames = "https://api.twitch.tv/helix/games/top";
         try {
-            //Pour nous service pour les autorisations d'identification pour twitch
-            const headers = new Headers();
-            headers.append("Authorization", authorization);
-            headers.append("Client-Id", clientId);
+            let url = urlGetTopGames;
+            yield createGames(url, authorization, clientId);
+        }
+        catch (error) {
+            console.log("ton fetch il est moyen : " + error);
+        }
+    });
+}
+//Pour les autorisation d'identification pour Twitch
+function sendTwitchRequest(url, authorization, clientId) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const headers = new Headers();
+        headers.append("Authorization", authorization);
+        headers.append("Client-Id", clientId);
+        //Utilise la requête et les autorisation
+        const response = yield fetch(url, { headers });
+        if (!response.ok) {
+            throw new Error("Response not OK ! Bolosse");
+        }
+        return response.json();
+    });
+}
+//Première requête pour ma collection Games
+function createGames(url, authorization, clientId) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
             //BOUCLE : pour parcourir les pages de résultats TANT QUE url!=null
             while (url) {
-                //Utilise la requête et les autorisation pour celle ci
-                const response = yield fetch(url, { headers });
-                let currentGame = new GameTopObject();
-                if (response.ok) {
-                    const game = yield response.json();
-                    if (game) {
-                        // Je récupère les données de mes jeux et je les stock dan ma gameArray
-                        for (let i = 0; i < game.data.length; i++) {
-                            currentGame.id = game.data[i].id;
-                            currentGame.name = game.data[i].name;
-                            currentGame.igdbId = game.data[i].igdb_id;
-                            //gameTopArray.push(currentGame);
-                            try {
-                                // Recherche du jeu existant avec l'ID actuel
-                                const existingGame = yield GameModel.findById(currentGame.id);
-                                if (existingGame) {
-                                    // Le jeu existe déjà, donc MAJ de la DB
-                                    existingGame.name = currentGame.name;
-                                    existingGame.igdbId = currentGame.igdbId;
-                                    yield existingGame.save();
-                                }
-                                else {
-                                    // Le jeu n'existe pas, création d'un nouveau game
-                                    const newGame = new GameModel({
-                                        _id: currentGame.id,
-                                        name: currentGame.name,
-                                        igdbId: currentGame.igdbId,
-                                    });
-                                    yield newGame.save();
-                                }
-                            }
-                            catch (error) {
-                                console.error("Erreur lors de l'enregistrement du jeu :", error);
-                            }
-                        }
-                    }
-                    //MAJ de l'url de la page suivante
-                    url =
-                        game.pagination.cursor !== null
-                            ? `${api}?first=100&after=${game.pagination.cursor}`
-                            : ""; // Récupère l'URL de la page suivante s'il y en a une
+                const game = yield sendTwitchRequest(url, authorization, clientId);
+                //Je récupère les données de mes jeux et je stock dans mon currentGame
+                for (let i = 0; i < game.data.length; i++) {
+                    const currentGame = {
+                        id: game.data[i].id,
+                        name: game.data[i].name,
+                        igdbId: game.data[i].igdb_id,
+                    };
+                    yield updateGame(currentGame);
                 }
-                else {
-                    throw new Error("Response not OK");
-                }
+                //MAJ de l'ur de la page suivante
+                url = game.pagination.cursor !== null ? `${url}?first=100&after=${game.pagination.cursor}` : "";
             }
         }
         catch (error) {
-            console.log("fetchGame foiré : " + error);
+            console.log("Ton Game Top il est pas ouf mais ça tu le sais : " + error);
         }
     });
 }
-function displayAPItwitch() {
+//Requête pour compléter la fiche du game avec les info de IGDB
+function editGamesWitchGetGame(url, authorization, clientId) {
+    return __awaiter(this, void 0, void 0, function* () {
+    });
+}
+//Méthode pour update ma database avec mon currentGame
+function updateGame(gameData) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const { id, name, igdbId } = gameData;
+        try {
+            //Recherche du game existant avec l'ID actuel
+            const existingGame = yield GameModel.findById(id);
+            if (existingGame) {
+                existingGame.name = name;
+                existingGame.igdbId = igdbId;
+                yield existingGame.save();
+            }
+            else {
+                //Le game n'existe pas donc création d'un nouveau
+                const newGame = new GameModel({
+                    _id: id,
+                    name,
+                    igdbId,
+                });
+                yield newGame.save();
+            }
+        }
+        catch (error) {
+            console.error("Erreur lors de l'enregistrement du game :", error);
+        }
+    });
+}
+function display() {
     return __awaiter(this, void 0, void 0, function* () {
         //J'ouvre le flux vers mongoDB et je remplis la db
         mongoose_1.default.connect("mongodb://127.0.0.1:27017/pixelhunt_db");
-        console.log("Connexion à MongoDB !");
+        console.log("Connexion à MongoDB avec le sourire !");
         yield fetchGame();
         mongoose_1.default.disconnect();
         console.log("On ferme MongoDB à clef svp");
-        //console.log(gameTopArray);
-        //console.log("La liste est grande sa mère : " + gameTopArray.length);
     });
 }
-displayAPItwitch();
+display();
--- easyBotTwitch.ts
+++ easyBotTwitch.ts
@@ -10,16 +10,6 @@
 */
 import mongoose from "mongoose";
 
-// J'ajoute cette ligne pour augmenter la limite d'écouteurs d'événements - SERT A RIEN POUR LE MOMENT
-//mongoose.connection.setMaxListeners(0);
-
-//Interface qui me sert pour mongoose
-// interface IGameTop {
-//   id: string;
-//   name: string;
-//   igdbId: string;
-// }
-
 //Je crée un schéma qui correspond à mon interface
 const gameSchema = new mongoose.Schema({
   _id: String,
@@ -35,95 +25,111 @@ class GameTopObject {
   constructor(
     public id?: string,
     public name?: string,
-    public igdbId?: string
+    public igdbId?: string, 
   ) {}
 }
 
-//ma petite liste de games pepouse
-//var gameTopArray: GameTopObject[] = [];
+//Méthode qui cherche mes Games et qui les enregistre sur MongoDB
+async function fetchGame(){
+    //Token pour l'API Twhitch et IGDB
+    const authorization = "Bearer 7tb61t29r3fhaft6ux6hh64fr1sf6v";
+    const clientId = "7xereixlp03cyd9lsebf4om6rensrb";
 
-async function fetchGame() {
-  // Pour twitch besoin d'Authorization et de clientId
-  let authorization = "Bearer 7tb61t29r3fhaft6ux6hh64fr1sf6v";
-  let clientId = "7xereixlp03cyd9lsebf4om6rensrb";
+    //Requête Get TopGames API Twitch
+    const urlGetTopGames = "https://api.twitch.tv/helix/games/top"
 
-  //La requête GET topGames
-  let api = "https://api.twitch.tv/helix/games/top";
-  let url = `${api}`;
 
-  try {
-    //Pour nous service pour les autorisations d'identification pour twitch
+    try{
+        let url = urlGetTopGames;
+        await createGames(url, authorization, clientId);
+
+    }catch (error){
+        console.log("ton fetch il est moyen : " + error);
+    }
+}
+
+//Pour les autorisation d'identification pour Twitch
+async function sendTwitchRequest(url: any, authorization: any, clientId: any){
     const headers = new Headers();
     headers.append("Authorization", authorization);
     headers.append("Client-Id", clientId);
 
-    //BOUCLE : pour parcourir les pages de résultats TANT QUE url!=null
-    while (url) {
-      //Utilise la requête et les autorisation pour celle ci
-      const response = await fetch(url, { headers });
-
-      let currentGame = new GameTopObject();
-      if (response.ok) {
-        const game = await response.json();
-
-        if (game) {
-          // Je récupère les données de mes jeux et je les stock dan ma gameArray
-          for (let i = 0; i < game.data.length; i++) {
-            currentGame.id = game.data[i].id;
-            currentGame.name = game.data[i].name;
-            currentGame.igdbId = game.data[i].igdb_id;
-            //gameTopArray.push(currentGame);
-
-            
-
-            try {
-                // Recherche du jeu existant avec l'ID actuel
-                const existingGame = await GameModel.findById(currentGame.id);
-  
-                if (existingGame) {
-                  // Le jeu existe déjà, donc MAJ de la DB
-                  existingGame.name = currentGame.name;
-                  existingGame.igdbId = currentGame.igdbId;
-  
-                  await existingGame.save();
-                } else {
-                  // Le jeu n'existe pas, création d'un nouveau game
-                  const newGame = new GameModel({
-                    _id: currentGame.id,
-                    name: currentGame.name,
-                    igdbId: currentGame.igdbId,
-                  });
-  
-                  await newGame.save();
-                }
-              } catch (error) {
-                console.error("Erreur lors de l'enregistrement du jeu :", error);
-              }
-            }
-          }
-        //MAJ de l'url de la page suivante
-        url =
-          game.pagination.cursor !== null
-            ? `${api}?first=100&after=${game.pagination.cursor}`
-            : ""; // Récupère l'URL de la page suivante s'il y en a une
-      } else {
-        throw new Error("Response not OK");
-      }
+    //Utilise la requête et les autorisation
+    const response = await fetch(url, {headers});
+    if(!response.ok){
+        throw new Error("Response not OK ! Bolosse");
     }
-  } catch (error) {
-    console.log("fetchGame foiré : " + error);
-  }
+    return response.json()
 }
 
-async function displayAPItwitch() {
+
+//Première requête pour ma collection Games
+async function createGames(url: any, authorization: any, clientId: any){
+    try{
+        //BOUCLE : pour parcourir les pages de résultats TANT QUE url!=null
+        while (url){
+
+            const game = await sendTwitchRequest(url, authorization, clientId);
+
+            //Je récupère les données de mes jeux et je stock dans mon currentGame
+            for (let i = 0; i < game.data.length; i++){
+                const currentGame = {
+                    id: game.data[i].id,
+                    name: game.data[i].name,
+                    igdbId: game.data[i].igdb_id,
+                }
+
+ 
+                await updateGame(currentGame);
+            }
+            //MAJ de l'ur de la page suivante
+            url = game.pagination.cursor !== null ? `${url}?first=100&after=${game.pagination.cursor}` : "";
+        }
+    }catch (error){
+        console.log("Ton Game Top il est pas ouf mais ça tu le sais : " + error);
+    }
+}
+
+//Requête pour compléter la fiche du game avec les info de IGDB
+async function editGamesWitchGetGame(url: any, authorization: any, clientId: any){
+
+
+
+}
+
+//Méthode pour update ma database avec mon currentGame
+async function updateGame(gameData: any) {
+    const { id, name, igdbId } = gameData;
+  
+    try {
+        //Recherche du game existant avec l'ID actuel
+      const existingGame = await GameModel.findById(id);
+  
+      if (existingGame) {
+        existingGame.name = name;
+        existingGame.igdbId = igdbId;
+        await existingGame.save();
+      } else {
+        //Le game n'existe pas donc création d'un nouveau
+        const newGame = new GameModel({
+          _id: id,
+          name,
+          igdbId,
+        });
+        await newGame.save();
+      }
+    } catch (error) {
+      console.error("Erreur lors de l'enregistrement du game :", error);
+    }
+  }
+
+async function display() {
     //J'ouvre le flux vers mongoDB et je remplis la db
     mongoose.connect("mongodb://127.0.0.1:27017/pixelhunt_db");
-    console.log("Connexion à MongoDB !");
+    console.log("Connexion à MongoDB avec le sourire !");
     await fetchGame();
     mongoose.disconnect();
     console.log("On ferme MongoDB à clef svp");
-  //console.log(gameTopArray);
-  //console.log("La liste est grande sa mère : " + gameTopArray.length);
 }
 
-displayAPItwitch();
+display();
